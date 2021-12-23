f0a95a67b14594aefb9bcdce29809b3c8479b453
Vladimir Ignatov <Vignat1002@gmail.com>
2021-12-23 20:07:05 +0300

    DZ_PY

--- py2.py
+++ py2.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 import os
 
-bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
+bash_command = ["cd ~/devops3-netology", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
-is_change = False
+#is_change = False
 for result in result_os.split('\n'):
-    if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', '')
+    if result.find('изменено') != -1:
+        prepare_result = result.replace('\tизменено:   ', '')
         print(prepare_result)
-        break
+#        break
